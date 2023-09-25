fd64f4470d0eb09c60c8321b01e720f6cb665da9
deepak-dpt <deepak.ramrakhyani@dream11.com>
2023-09-25 19:15:59 +0530

    WIP

--- package/example/src/CameraPage.tsx
+++ package/example/src/CameraPage.tsx
@@ -32,7 +32,7 @@ export function CameraPage({ navigation }: Props): React.ReactElement {
   const [isCameraInitialized, setIsCameraInitialized] = useState(false);
   const [readQr, setReadQr] = useState(true);
   const [qrResult, setQrResult] = useState('');
-  const [hasMicrophonePermission, setHasMicrophonePermission] = useState(false);
+  const [_, setHasMicrophonePermission] = useState(false);
   const zoom = useSharedValue(0);
   const isPressingButton = useSharedValue(false);
 
@@ -51,8 +51,18 @@ export function CameraPage({ navigation }: Props): React.ReactElement {
     physicalDevices: ['ultra-wide-angle-camera', 'wide-angle-camera', 'telephoto-camera'],
   });
 
+  const aspectRatio3by4 = 3 / 4;
+  const aspectRatio9by16 = 9 / 16;
+
   const format = useCameraFormat(device, [
-    { fps: 60 }, //
+    {
+      fps: 60,
+      videoResolution: {
+        width: 1080,
+        height: 1920,
+      },
+      videoAspectRatio: aspectRatio3by4,
+    }, //
   ]);
 
   //#region Memos
@@ -162,6 +172,7 @@ export function CameraPage({ navigation }: Props): React.ReactElement {
     if (result !== 'nope') setBarcodeJs(result);
 
     console.log('QRReader result', result);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -193,6 +204,7 @@ export function CameraPage({ navigation }: Props): React.ReactElement {
                 style={StyleSheet.absoluteFill}
                 device={device}
                 pixelFormat={'yuv'}
+                resizeMode={'contain'}
                 // format={device.formats.find((f) => f.pixelFormats.includes('yuv'))}
                 fps={fps}
                 //hdr={enableHdr}
@@ -207,7 +219,7 @@ export function CameraPage({ navigation }: Props): React.ReactElement {
                 photo={true}
                 //video={true}
                 //audio={hasMicrophonePermission}
-                frameProcessor={readQr ? frameProcessor : null}
+                frameProcessor={readQr ? frameProcessor : undefined}
               />
             </TapGestureHandler>
           </Reanimated.View>
